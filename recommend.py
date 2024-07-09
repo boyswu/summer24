f2b66508b7984f6090bd628cdc9e841e1cc9e7c5
boys_w <boys_w@noreply.gitcode.com>
2024-07-09 20:29:26 +0800

    第八次修改

--- recommend.py
+++ recommend.py
@@ -39,24 +39,31 @@ def get_recommendations(title, cosine_sim, books_df):
     """
     # 找到与输入书籍最相似的书籍
     try:
+        # 找到输入书籍的索引
         idx = books_df[books_df['title'] == title].index[0]
+        # 找到最相似的书籍的索引
         sim_scores = list(enumerate(cosine_sim[idx]))
+        # 按相似度排序
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        sim_scores = sim_scores[1:6]
+
+        sim_scores = sim_scores[1:15]
         book_indices = [i[0] for i in sim_scores]
         return books_df.loc[book_indices, ['title', 'genre']]
     except IndexError:
         print(f"书籍 '{title}' 未找到在数据库中。")
         return []
 
-
 def recommend_books(user_id):
     books_df, borrow_df = connect_sql()
+    # print(books_df)
+    # print(borrow_df)
     # 数据预处理
     if books_df is not None or borrow_df is not None:
 
         books_df.drop_duplicates(subset=['book_id'], inplace=True)
         borrow_df.drop_duplicates(inplace=True)
+        # print(books_df)
+        # print(borrow_df)
     # 使用基于内容的推荐算法
     tfidf = TfidfVectorizer(stop_words='english')
     # 填充缺失值
@@ -71,7 +78,6 @@ def recommend_books(user_id):
 
     if user_id in borrow_df['user_id'].values:
         user_books = borrow_df[borrow_df['user_id'] == user_id]['book_id'].tolist()
-        # print(f"用户{user_id}的借阅书籍：{user_books}")
     else:
         user_books = []
         print(f"No books found for user_id: {user_id}")
@@ -80,16 +86,16 @@ def recommend_books(user_id):
         print(f"用户{user_id}没有借阅记录。")
 
     user_book_titles = books_df[books_df['book_id'].isin(user_books)]['title'].tolist()
-
+    print(f"用户{user_id}的借阅书籍：{user_book_titles}")
+    book_info = []
     for title in user_book_titles:
-
         recommend_book = get_recommendations(title, cosine_sim, books_df)
         book_info = recommend_book[['title', 'genre']].apply(lambda x: f"书名: {x['title']}, 类别: {x['genre']}",
                                                              axis=1)
-        for info in book_info:
-            print(info)
-        print("\n")
-        return book_info
+    for info in book_info:
+        print(info)
+    print("\n")
+    return book_info
 
 
 if __name__ == '__main__':
